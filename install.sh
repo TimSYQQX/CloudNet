b72824a7cfeafb05c29afc17fc3e5d6bedf890dc
xiongti1 <tianyuxiong1997@gmail.com>
2019-11-20 02:54:16 -0500

    update

---
+++ install.sh
@@ -0,0 +1,8 @@
+conda install nodejs
+pip install catalyst
+pip install ipympl
+pip install albumentations
+pip install pretrainedmodels
+pip install git+https://github.com/qubvel/segmentation_models.pytorch
+pip install pytorch_toolbelt
+pip install torchvision==0.4
